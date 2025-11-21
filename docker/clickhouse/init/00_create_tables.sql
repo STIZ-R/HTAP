69b486597efb210ea5fa3c94bf773b52f3f99973
etudiant <etudiant@debian.iem>
2025-11-21 10:04:11 +0100

    Lire le O;txt pour comprendre

--- docker/clickhouse/init/00_create_tables.sql
+++ docker/clickhouse/init/00_create_tables.sql
@@ -25,7 +25,7 @@ CREATE TABLE IF NOT EXISTS users (
     _version UInt64,      -- Debezium ts_ms
     _deleted UInt8        -- 1 si delete, 0 sinon
 )
-ENGINE = ReplacingMergeTree(id)
+ENGINE = ReplacingMergeTree(_version)
 ORDER BY id;
 
 -- -------------------------
@@ -40,5 +40,5 @@ CREATE TABLE IF NOT EXISTS orders (
     _version UInt64,
     _deleted UInt8
 )
-ENGINE = ReplacingMergeTree(id)
+ENGINE = ReplacingMergeTree(_version)
 ORDER BY id;
